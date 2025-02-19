adc9ba2cc1efc889127e75d21e6ba91d45b4b2df
Ahmd-Khaled <ahmd.khaldd@gmail.com>
2025-02-19 10:33:45 +0200

    Update Media Queries-3

--- components/Dashboard/AvatarMobile/AvatarMobile.jsx
+++ components/Dashboard/AvatarMobile/AvatarMobile.jsx
@@ -1,7 +1,52 @@
+"use client";
+import Image from "next/image";
 import styles from "./styles.module.scss";
-
+import { useEffect, useRef, useState } from "react";
+import { FaBars } from "react-icons/fa6";
 const AvatarMobile = () => {
-  return <div className={styles.avatarMobile}>AvatarMobile</div>;
+  const btnRef = useRef();
+  const listRef = useRef();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const openOptionHandler = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (
+        !btnRef.current?.contains(e.target) &&
+        !listRef.current?.contains(e.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      if (isOpen) {
+        document.removeEventListener("mousedown", handler);
+      }
+    };
+  }, []);
+  return (
+    <div className={styles.avatarMob}>
+      <button className={styles.avBtn} onClick={openOptionHandler} ref={btnRef}>
+        <div className={styles.avImage}>
+          <Image src="/images/user.jpg" alt="user" width={42} height={42} />
+        </div>
+        <div className={styles.avIcon}>
+          <FaBars />
+        </div>
+      </button>
+      {isOpen ? (
+        <div className={styles.profDropdown} ref={listRef}>
+          <ProfileDropDownMenu />
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default AvatarMobile;
