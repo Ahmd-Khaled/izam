847532136f6cd35b3b1be3a1bdc2adad27074228
Ahmd-Khaled <ahmd.khaldd@gmail.com>
2025-02-20 02:36:59 +0200

    Add Dragable SubLinks -2

--- components/Dashboard/NavBarContainer/NavBarContainer.jsx
+++ components/Dashboard/NavBarContainer/NavBarContainer.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import NavBarDragDrop from "../NavBarDragDrop/NavBarDragDrop";
 import NavBarHead from "../NavBarHead/NavBarHead";
 import NavBarList from "../NavBarList/NavBarList";
-import LongPressWrapper from "@/components/utils/LongPressButton/LongPressButton";
 import useGetNavList from "@/hooks/NavBar/useGetNavList";
 
 const NavBarContainer = ({ close }) => {
@@ -26,18 +25,19 @@ const NavBarContainer = ({ close }) => {
 
   return (
     <div className={styles.navContainer}>
-      <LongPressWrapper onLongPress={handleToggleDragDropList}>
-        <NavBarHead
-          stepBackHandler={stepBackHandler}
-          isDaragDropOpen={isDaragDropOpen}
-          handleToggleDragDropList={handleToggleDragDropList}
-        />
-      </LongPressWrapper>
+      <NavBarHead
+        stepBackHandler={stepBackHandler}
+        isDaragDropOpen={isDaragDropOpen}
+        handleToggleDragDropList={handleToggleDragDropList}
+      />
       <div className={styles.navListsWraper}>
         {isDaragDropOpen ? (
           <NavBarDragDrop navList={navList} close={handleToggleDragDropList} />
         ) : (
-          <NavBarList navList={navList} />
+          <NavBarList
+            navList={navList}
+            openEditMode={handleToggleDragDropList}
+          />
         )}
       </div>
     </div>
--- components/Dashboard/NavBarList/NavBarList.jsx
+++ components/Dashboard/NavBarList/NavBarList.jsx
@@ -3,8 +3,9 @@ import styles from "./styles.module.scss";
 import Link from "next/link";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 import { useState } from "react";
+import LongPressWrapper from "@/components/utils/LongPressButton/LongPressButton";
 
-const NavBarList = ({ navList }) => {
+const NavBarList = ({ navList, openEditMode }) => {
   const [isMainLinkOpen, setIsMainLinkOpen] = useState(false);
   const [clickedMainLink, setClickedMainLink] = useState(null);
 
@@ -15,57 +16,59 @@ const NavBarList = ({ navList }) => {
 
   return (
     <div className={styles.navBarList}>
-      <ul className={styles.navList}>
-        {navList?.map((item) => (
-          <li key={item?.id} className={styles.navItem}>
-            <div className={styles.navMainLink}>
-              <Link
-                href={
-                  item?.title === "Qualifications"
-                    ? "/qualifications"
-                    : item?.target
-                }
-              >
-                <h4>{item?.title}</h4>
-              </Link>
-
-              {item?.children && item?.children?.length > 0 ? (
-                <button
-                  className={styles.subLinkBtn}
-                  onClick={() => handleToggleMainLink(item?.id)}
+      <LongPressWrapper onLongPress={openEditMode}>
+        <ul className={styles.navList}>
+          {navList?.map((item) => (
+            <li key={item?.id} className={styles.navItem}>
+              <div className={styles.navMainLink}>
+                <Link
+                  href={
+                    item?.title === "Qualifications"
+                      ? "/qualifications"
+                      : item?.target
+                  }
                 >
-                  {isMainLinkOpen && clickedMainLink === item?.id ? (
-                    <FaAngleUp />
-                  ) : (
-                    <FaAngleDown />
-                  )}
-                </button>
+                  <h4>{item?.title}</h4>
+                </Link>
+
+                {item?.children && item?.children?.length > 0 ? (
+                  <button
+                    className={styles.subLinkBtn}
+                    onClick={() => handleToggleMainLink(item?.id)}
+                  >
+                    {isMainLinkOpen && clickedMainLink === item?.id ? (
+                      <FaAngleUp />
+                    ) : (
+                      <FaAngleDown />
+                    )}
+                  </button>
+                ) : null}
+              </div>
+              {item?.children &&
+              item?.children?.length > 0 &&
+              isMainLinkOpen &&
+              clickedMainLink === item?.id ? (
+                <ul className={styles.navSubLinks}>
+                  {item?.children?.map((child) => (
+                    <li key={child?.id} className={styles.navSubItem}>
+                      <Link
+                        className={styles.navSubLink}
+                        href={
+                          item?.title === "Qualifications"
+                            ? `/qualifications${child?.target}`
+                            : child?.target
+                        }
+                      >
+                        <h3>{child?.title}</h3>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               ) : null}
-            </div>
-            {item?.children &&
-            item?.children?.length > 0 &&
-            isMainLinkOpen &&
-            clickedMainLink === item?.id ? (
-              <ul className={styles.navSubLinks}>
-                {item?.children?.map((child) => (
-                  <li key={child?.id} className={styles.navSubItem}>
-                    <Link
-                      className={styles.navSubLink}
-                      href={
-                        item?.title === "Qualifications"
-                          ? `/qualifications${child?.target}`
-                          : child?.target
-                      }
-                    >
-                      <h3>{child?.title}</h3>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            ) : null}
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </LongPressWrapper>
     </div>
   );
 };
